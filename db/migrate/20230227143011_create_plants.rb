49d35725342313f24cedd28ccd202ec07efe33de
canozben <canozben@windowslive.com>
2023-02-27 15:41:37 +0100

    models

---
+++ db/migrate/20230227143011_create_plants.rb
@@ -0,0 +1,13 @@
+class CreatePlants < ActiveRecord::Migration[7.0]
+  def change
+    create_table :plants do |t|
+      t.string :title
+      t.text :description
+      t.float :price
+      t.string :city
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
