9390f3045c6c01569b59104e33e6563e43811b63
canozben <canozben@windowslive.com>
2023-02-27 17:26:16 +0100

    plants controller and views done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = nil
+5.times do
+  email = Faker::Internet.email
+  user = User.new(email: email)
+end
+
+5.times do
+  title = Faker::Games::WorldOfWarcraft.hero
+  description = Faker::Games::WorldOfWarcraft.quote
+  price = rand(1..10)
+  city = Faker::Address.city
+  plant = Plant.new(title: title, description: description, price: price, city: city, user: user)
+  plant.user = User.all.sample
+  plant.save!
+end
